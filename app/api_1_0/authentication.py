a67d7d61ac22bcbf55d335c0c3e463821167293a
chenke91 <chenke91@qq.com>
2014-10-27 11:49:47 +0800

    api

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -1,7 +1,8 @@
 from flask import g, jsonify
 from ..models import AnonymousUser, User
 from flask.ext.httpauth import HTTPBasicAuth
-from .errors import forbidden
+from .errors import forbidden, unauthorized
+from . import api
 
 auth = HTTPBasicAuth()
 
@@ -12,23 +13,19 @@ def verify_password(email_or_token, password):
         return True
     if password == '':
         g.current_user = User.verify_auth_token(email_or_token)
-        g.token_userd = True
+        g.token_used = True
         return g.current_user is not None
     user = User.query.filter_by(email=email_or_token).first()
-    if not User:
+    if not user:
         return False
     g.current_user = user
-    g.token_userd = False
+    g.token_used = False
     return user.verify_password(password)
 
 @auth.error_handler
 def auth_error():
     return unauthorized('Invalid credentials')
 
-@api.route('/post/')
-@auth.login_required
-def get_posts():
-    pass
 
 @api.before_request
 @auth.login_required
